4a010bc32fdf2f9e3cd3cd2bf5b27578bc9e7e0a
JaceHorn <111376299+JaceHorn@users.noreply.github.com>
2022-08-22 08:51:10 -0500

    First Python Program

---
+++ first.py
@@ -0,0 +1,6 @@
+#programmer : Tom with Jerry
+#program : Hello World:following the traditon
+#version : Python 3. 8. 2
+#First writtin : 08- 22- 2022 8:46:00 AM
+# Last updated: 08-22-2022 8:47:00 AM
+print ("Hello World")
