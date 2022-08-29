7692fcfbad118cbfe725276484ccfc3321984811
JaceHorn <111376299+JaceHorn@users.noreply.github.com>
2022-08-29 08:49:44 -0500

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+Arithmetic operations
+sum of given numbers is 15
+difference of given numbers is -5
+remainder of given numbers is 5
+quotiant of given number is 0.5
+product of given numbers is 50
+> 
